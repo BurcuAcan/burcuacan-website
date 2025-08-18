d3df51c7b13262b89a7d8fc516014997f806d17f
Burcu Açan <burcuuacann@gmail.com>
2025-08-18 10:56:40 +0300

    fix: viewpor value about mobile view

--- src/components/molecules/AnimatedSection.tsx
+++ src/components/molecules/AnimatedSection.tsx
@@ -16,7 +16,7 @@ const AnimatedSection = ({ children, className, id }: AnimatedSectionProps) => {
       className={className}
       initial={{ opacity: 0, y: 50 }}
       whileInView={{ opacity: 1, y: 0 }}
-      viewport={{ once: true, amount: 0.3 }}
+      viewport={{ once: true, amount: 0.1 }}
       transition={{ duration: 0.5 }}
     >
       {children}
