8599c203ee9ae7da9c23df896cb860265026ed19
Burcu Açan <burcuuacann@gmail.com>
2025-08-18 11:06:05 +0300

    fix: viewport for animation

--- src/components/molecules/AnimatedSection.tsx
+++ src/components/molecules/AnimatedSection.tsx
@@ -16,7 +16,7 @@ const AnimatedSection = ({ children, className, id }: AnimatedSectionProps) => {
       className={className}
       initial={{ opacity: 0, y: 50 }}
       whileInView={{ opacity: 1, y: 0 }}
-      viewport={{ once: true, amount: 0.1, margin: "-100px" }}
+      viewport={{ once: true, amount: 0.1, margin: "-50px" }}
       transition={{ duration: 0.5 }}
     >
       {children}
